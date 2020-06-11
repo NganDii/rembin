7001771b9ae1b4d2f2bcd8781138c091693b2e1c
RemApple <32689703+NganDii@users.noreply.github.com>
2020-06-11 16:16:06 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,46 +1,13 @@
-echo "Choose your device: "
-echo "1.  Phone"
-echo "2.  Computer"
-read -r option
-if [ $option == 1 ]; then
-cd ~
-rm -rf temp
-mkdir temp
-cd /sdcard/InsBin/
-cp url ins.sh ~/temp/
-printf "\n\n\n"
-echo -e "###### Enter your bin file name ####### \e[1;92m"
-read -r name
-clear
-printf "\n\n\n"
-echo -e "\e[1m ################### COPY THIS... #################"
-echo ""
-echo -e "\e[93;1mcp -f url /data/data/com.termux/files/usr/bin/$name && chmod +x /data/data/com.termux/files/usr/bin/$name \e[0m"
-printf "\n\nWait.....\n\n\n\n"
-sleep 30
-echo "A new Dialog will show up...Paste there and Press Ctrl+x and hit y and Enter"
-sleep 30
-echo ""
-echo ""
-cd ~/temp/
-nano ins.sh
-bash ins.sh
-echo -e "\e[102mSUCCESSFUL \e[0m"
-echo ""
-echo "" 
-echo ""
-cd ~
-rm -rf temp
-elif [ $option == 2 ]; then
-echo "Computer"
+if [ ! -e "~/rembin" ]; then
 cd ~/ && mkdir rembin && cd rembin
+fi
 echo -e "###### Enter your bin file name ####### \e[1;92m"
 read -r name
 clear
 printf "\n\n\Enter your codes for bin file(Ctrl+x, y and hit enter to save): "
 nano url
 cat <<EOF>bin.sh
-cp -f url /usr/bin/$name && chmod +x /usr/bin/$name
+sudo cp -f url /usr/bin/$name && sudo chmod +x /usr/bin/$name
 EOF
 bash bin.sh
 echo -e "\e[102mSUCCESSFUL \e[0m"
@@ -48,7 +15,3 @@ echo ""
 echo "" 
 echo ""
 cd ~
-else
-echo "Invalid choice"
-exit 0
-fi
