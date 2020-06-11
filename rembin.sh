1c60f6fe3d9863cdf1077f2b16eabe73ff121f67
RemApple <32689703+NganDii@users.noreply.github.com>
2020-06-11 17:06:21 +0530

    Update rembin.sh

--- rembin.sh
+++ rembin.sh
@@ -1,16 +1,11 @@
-
-
-
-cp ~/rembin/Install.sh ~/
-mv ~/Install.sh ~/url
 printf "\nChoose your devices\n\n1.   Phone\n2.    Computer\n\n"
 read -r option
 if [ $option == 1 ]; then
-cp -f ~/url /data/data/com.termux/files/usr/bin/rembin && chmod +x /data/data/com.termux/files/usr/bin/rembin
+cp -f ~/rembin/url /data/data/com.termux/files/usr/bin/rembin && chmod +x /data/data/com.termux/files/usr/bin/rembin
 elif [ $option == 2 ]; then
-sudo cp -f ~/url /usr/bin/rembin && sudo chmod +x /usr/bin/rembin
+sudo cp -f ~/rembin/url /usr/bin/rembin && sudo chmod +x /usr/bin/rembin
 else
 echo "Invalid Option"
 exit 1
 fi
-rm ~/url
+rm -rf ~/rembin
